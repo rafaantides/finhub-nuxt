3d7dd593750f615c06a716311a3dbaa8a9ae8040
RafaelAntides <rafaantides@gmail.com>
2025-04-19 18:28:34 -0300

    header

--- app/components/DataTable.vue
+++ app/components/DataTable.vue
@@ -28,67 +28,51 @@ const pagination = ref({
 </script>
 
 <template>
-  <UDashboardPanel id="debts">
-    <template #header>
-      <UDashboardNavbar title="Débitos">
-        <template #leading>
-          <UDashboardSidebarCollapse />
-        </template>
+  <div class="flex flex-wrap items-center justify-between gap-1.5">
+    <UInput
+      :model-value="(table?.tableApi?.getColumn('title')?.getFilterValue() as string)"
+      class="max-w-sm"
+      icon="i-lucide-search"
+      placeholder="Filter..."
+      @update:model-value="(value) => (search = String(value))"
+    />
 
-        <template #right>
-          <DebtsAddModal />
-        </template>
-      </UDashboardNavbar>
-    </template>
+    <div class="flex flex-wrap items-center gap-1.5">
+      <CustomersDeleteModal
+        :count="table?.tableApi?.getFilteredSelectedRowModel().rows.length"
+      >
+        <UButton
+          v-if="table?.tableApi?.getFilteredSelectedRowModel().rows.length"
+          label="Delete"
+          color="error"
+          variant="subtle"
+          icon="i-lucide-trash"
+        >
+          <template #trailing>
+            <UKbd>
+              {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length }}
+            </UKbd>
+          </template>
+        </UButton>
+      </CustomersDeleteModal>
 
-    <template #body>
-      <div class="flex flex-wrap items-center justify-between gap-1.5">
-        <UInput
-          :model-value="(table?.tableApi?.getColumn('title')?.getFilterValue() as string)"
-          class="max-w-sm"
-          icon="i-lucide-search"
-          placeholder="Filter..."
-          @update:model-value="(value) => (search = String(value))"
-        />
-
-        <div class="flex flex-wrap items-center gap-1.5">
-          <CustomersDeleteModal
-            :count="table?.tableApi?.getFilteredSelectedRowModel().rows.length"
-          >
-            <UButton
-              v-if="table?.tableApi?.getFilteredSelectedRowModel().rows.length"
-              label="Delete"
-              color="error"
-              variant="subtle"
-              icon="i-lucide-trash"
-            >
-              <template #trailing>
-                <UKbd>
-                  {{
-                    table?.tableApi?.getFilteredSelectedRowModel().rows.length
-                  }}
-                </UKbd>
-              </template>
-            </UButton>
-          </CustomersDeleteModal>
-
-          <USelect
-            v-model="statusFilter"
-            :items="[
-              { label: 'All', value: 'all' },
-              { label: 'Subscribed', value: 'subscribed' },
-              { label: 'Unsubscribed', value: 'unsubscribed' },
-              { label: 'Bounced', value: 'bounced' }
-            ]"
-            :ui="{
-              trailingIcon:
-                'group-data-[state=open]:rotate-180 transition-transform duration-200'
-            }"
-            placeholder="Filter status"
-            class="min-w-28"
-          />
-          <UDropdownMenu
-            :items="
+      <USelect
+        v-model="statusFilter"
+        :items="[
+          { label: 'All', value: 'all' },
+          { label: 'Subscribed', value: 'subscribed' },
+          { label: 'Unsubscribed', value: 'unsubscribed' },
+          { label: 'Bounced', value: 'bounced' }
+        ]"
+        :ui="{
+          trailingIcon:
+            'group-data-[state=open]:rotate-180 transition-transform duration-200'
+        }"
+        placeholder="Filter status"
+        class="min-w-28"
+      />
+      <UDropdownMenu
+        :items="
           table?.tableApi
             ?.getAllColumns()
             .filter((column: Column<any, unknown>) => column.getCanHide())
@@ -104,54 +88,52 @@ const pagination = ref({
               }
             }))
         "
-            :content="{ align: 'end' }"
-          >
-            <UButton
-              label="Display"
-              color="neutral"
-              variant="outline"
-              trailing-icon="i-lucide-settings-2"
-            />
-          </UDropdownMenu>
-        </div>
-      </div>
-      <UTable
-        ref="table"
-        v-model:column-filters="columnFilters"
-        v-model:column-visibility="columnVisibility"
-        v-model:row-selection="rowSelection"
-        v-model:pagination="pagination"
-        class="shrink-0"
-        :data="data"
-        :columns="columns"
-        :loading="loading"
-        :ui="{
-          base: 'table-fixed border-separate border-spacing-0',
-          thead: '[&>tr]:bg-(--ui-bg-elevated)/50 [&>tr]:after:content-none',
-          tbody: '[&>tr]:last:[&>td]:border-b-0',
-          th: 'py-1 first:rounded-l-[calc(var(--ui-radius)*2)] last:rounded-r-[calc(var(--ui-radius)*2)] border-y border-(--ui-border) first:border-l last:border-r',
-          td: 'border-b border-(--ui-border)'
-        }"
-      />
-
-      <div
-        class="flex items-center justify-between gap-3 border-t border-(--ui-border) pt-4 mt-auto"
+        :content="{ align: 'end' }"
       >
-        <div class="text-sm text-(--ui-text-muted)">
-          {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length || 0 }}
-          of
-          {{ table?.tableApi?.getFilteredRowModel().rows.length || 0 }}
-          row(s) selected.
-        </div>
+        <UButton
+          label="Display"
+          color="neutral"
+          variant="outline"
+          trailing-icon="i-lucide-settings-2"
+        />
+      </UDropdownMenu>
+    </div>
+  </div>
+  <UTable
+    ref="table"
+    v-model:column-filters="columnFilters"
+    v-model:column-visibility="columnVisibility"
+    v-model:row-selection="rowSelection"
+    v-model:pagination="pagination"
+    class="shrink-0"
+    :data="data"
+    :columns="columns"
+    :loading="loading"
+    :ui="{
+      base: 'table-fixed border-separate border-spacing-0',
+      thead: '[&>tr]:bg-(--ui-bg-elevated)/50 [&>tr]:after:content-none',
+      tbody: '[&>tr]:last:[&>td]:border-b-0',
+      th: 'py-1 first:rounded-l-[calc(var(--ui-radius)*2)] last:rounded-r-[calc(var(--ui-radius)*2)] border-y border-(--ui-border) first:border-l last:border-r',
+      td: 'border-b border-(--ui-border)'
+    }"
+  />
 
-        <div class="flex items-center gap-1.5">
-          <UPagination
-            v-model:page="currentPage"
-            :items-per-page="pageSize"
-            :total="total"
-          />
-        </div>
-      </div>
-    </template>
-  </UDashboardPanel>
+  <div
+    class="flex items-center justify-between gap-3 border-t border-(--ui-border) pt-4 mt-auto"
+  >
+    <div class="text-sm text-(--ui-text-muted)">
+      {{ table?.tableApi?.getFilteredSelectedRowModel().rows.length || 0 }}
+      of
+      {{ table?.tableApi?.getFilteredRowModel().rows.length || 0 }}
+      row(s) selected.
+    </div>
+
+    <div class="flex items-center gap-1.5">
+      <UPagination
+        v-model:page="currentPage"
+        :items-per-page="pageSize"
+        :total="total"
+      />
+    </div>
+  </div>
 </template>
--- app/pages/debts.vue
+++ app/pages/debts.vue
@@ -6,8 +6,6 @@ import {
   debtGetRowItems
 } from '~/composables/table/useDebtTableColumns'
 
-// const statusFilter = ref('all')
-
 const components = {
   UBadge: resolveComponent('UBadge') as Component,
   UCheckbox: resolveComponent('UCheckbox') as Component,
@@ -35,27 +33,31 @@ const columns = useTableColumns(
   debtGetRowItems,
   components
 )
-
-// watch(statusFilter, (newVal) => {
-//   const statusColumn = table.value?.tableApi?.getColumn('status')
-//   if (!statusColumn) return
-//   statusColumn.setFilterValue(newVal === 'all' ? undefined : newVal)
-// })
 </script>
 
 <template>
   <UDashboardPanel id="debts">
-    <DataTable
-      v-model:current-page="currentPage"
-      v-model:page-size="pageSize"
-      v-model:search="search"
-      :data="data"
-      :columns="columns"
-      :loading="status === 'pending'"
-      :total="total"
-      @update:current-page="(val) => (currentPage = val)"
-    />
+    <template #header>
+      <UDashboardNavbar title="Débitos">
+        <template #leading>
+          <UDashboardSidebarCollapse />
+        </template>
+        <template #right>
+          <DebtsAddModal />
+        </template>
+      </UDashboardNavbar>
+    </template>
+    <template #body>
+      <DataTable
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        v-model:search="search"
+        :data="data"
+        :columns="columns"
+        :loading="status === 'pending'"
+        :total="total"
+        @update:current-page="(val) => (currentPage = val)"
+      />
+    </template>
   </UDashboardPanel>
 </template>
-
-<!-- :status-filter="statusFilter" -->
