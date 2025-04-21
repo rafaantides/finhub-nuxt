ef9f212c3e4d9900f8b76bd2b08b8cf8e53dc940
RafaelAntides <rafaantides@gmail.com>
2025-04-21 20:12:09 -0300

    HomeChart.client

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
   rules: {
     'vue/no-multiple-template-root': 'off',
-    'vue/max-attributes-per-line': ['error', { singleline: 3 }],
+    // 'vue/max-attributes-per-line': ['error', { singleline: 3 }],
     '@stylistic/quote-props': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@stylistic/arrow-parens': ['error', 'always'],
@@ -14,6 +14,7 @@ export default withNuxt({
     'vue/operator-linebreak': 'off',
     'vue/html-indent': 'off',
     'vue/html-self-closing': 'off',
-    'vue/singleline-html-element-content-newline': 'off'
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/max-attributes-per-line': 'off'
   }
 })
